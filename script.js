1c3dc443b8478f9c0563239ee470b972abdb0f61
Hetphil <p_bordmann@orange.fr>
2017-02-14 10:39:12 +0100

    -mise en page

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var map;
-function initMap() {
+function initMap()
+{
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 48.862725, lng: 2.287592000000018},
         zoom: 11
